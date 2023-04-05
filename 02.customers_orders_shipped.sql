85f01c2aeaa6094d59b77a24465aa2201b4e1125
daniel <danielrubensdaniel@gmail.com>
2023-04-05 12:54:04 -0300

    customer orders shipped: add year equals to 2003

--- 02.customers_orders_shipped.sql
+++ 02.customers_orders_shipped.sql
@@ -1,6 +1,7 @@
--- SELECT customerNumber FROM classicmodels.orders WHERE MONTH(orderDate) = 8;
+-- SELECT customerNumber FROM classicmodels.orders WHERE YEAR(orderDate) = 2003 AND MONTH(orderDate) = 8;
 
 SELECT DISTINCT c.* 
 FROM classicmodels.customers c 
 INNER JOIN classicmodels.orders o ON c.customerNumber = o.customerNumber 
-WHERE MONTH(o.orderDate) = 8;
+WHERE YEAR(o.orderDate) = 2003 AND MONTH(o.orderDate) = 8;
+
