62e82cce13d91077c311aa500b5510e5aa4782a6
daniel <danielrubensdaniel@gmail.com>
2023-04-09 03:13:56 -0300

    fix: tests with false positive

--- tests/classicmodels.spec.js
+++ tests/classicmodels.spec.js
@@ -2,8 +2,7 @@ const { readFileSync } = require('fs');
 const { Sequelize }  = require('sequelize');
 const Importer = require('mysql-import');
 
-const { MYSQL_USER, MYSQL_PASSWORD, HOSTNAME, PORT } = process.env;
-
+const { MYSQL_USER, MYSQL_PASSWORD, HOSTNAME, PORT} = process.env
 describe('MySQL Sample Database Tests', () => {
     let sequelize;
 
@@ -12,6 +11,7 @@ describe('MySQL Sample Database Tests', () => {
             {user: MYSQL_USER, password: MYSQL_PASSWORD, host: HOSTNAME, port: PORT}
         );
         await importer.import('./classicmodels.sql');
+        
         importer.disconnect();
         sequelize = new Sequelize('classicmodels', MYSQL_USER, MYSQL_PASSWORD, 
             { host: HOSTNAME, port: PORT, dialect: 'mysql'});
@@ -22,22 +22,22 @@ describe('MySQL Sample Database Tests', () => {
         sequelize.close()
     })
 
-    it('Return all Employees who report to Anthony Bow, Sales Manager', async () => {
-        const query = readFileSync('01.employees_report.sql', 'utf-8');
+    it('Return all Employees who report to Bow Antony, Sales Manager', async () => {
+        const query = readFileSync('01.employees_report.sql', 'utf-8').trim();
         const result = require('./results/01.employees_report.json')
-        expect(await sequelize.query(query, { type: 'SELECT' })).toEqual(result)
+        expect(await sequelize.query(query, { type: 'SELECT'})).toEqual(result)
     })
     
     it('Find all Customers and theirs orders that were shipped in August 2003', async () => {
-        const query = readFileSync('02.customers_orders_shipped.sql', 'utf-8');
+        const query = readFileSync('02.customers_orders_shipped.sql', 'utf-8').trim();
         const result = require('./results/02.customers_orders_shipped.json')
-        expect(await sequelize.query(query, { type: 'SELECT' })).toEqual(result)
+        expect(await sequelize.query(query, { type: 'SELECT'})).toEqual(result)
     })
 
     it('List only the products with orders where products have been ordered by the price of 100 to 150', async () => {
-        const query = readFileSync('03.products_orders_price.sql', 'utf-8');
+        const query = readFileSync('03.products_orders_price.sql', 'utf-8').trim();
         const result = require('./results/03.products_orders_price.json')
-        expect(await sequelize.query(query, { type: 'SELECT' })).toEqual(result)
+        expect(await sequelize.query(query, { type: 'SELECT'})).toEqual(result)
     })
     
 })
