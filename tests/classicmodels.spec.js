c46a89b50b348ccfc4f8b62a201a4087393b4dfd
daniel <danielrubensdaniel@gmail.com>
2023-04-09 01:07:10 -0300

    remove unecessary packages

--- tests/classicmodels.spec.js
+++ tests/classicmodels.spec.js
@@ -23,19 +23,19 @@ describe('MySQL Sample Database Tests', () => {
     })
 
     it('Return all Employees who report to Anthony Bow, Sales Manager', async () => {
-        const query = readFileSync('01.employees_report.sql', 'utf-8').trim();
+        const query = readFileSync('01.employees_report.sql', 'utf-8');
         const result = require('./results/01.employees_report.json')
         expect(await sequelize.query(query, { type: 'SELECT' })).toEqual(result)
     })
     
     it('Find all Customers and theirs orders that were shipped in August 2003', async () => {
-        const query = readFileSync('02.customers_orders_shipped.sql', 'utf-8').trim();
+        const query = readFileSync('02.customers_orders_shipped.sql', 'utf-8');
         const result = require('./results/02.customers_orders_shipped.json')
         expect(await sequelize.query(query, { type: 'SELECT' })).toEqual(result)
     })
 
     it('List only the products with orders where products have been ordered by the price of 100 to 150', async () => {
-        const query = readFileSync('03.products_orders_price.sql', 'utf-8').trim();
+        const query = readFileSync('03.products_orders_price.sql', 'utf-8');
         const result = require('./results/03.products_orders_price.json')
         expect(await sequelize.query(query, { type: 'SELECT' })).toEqual(result)
     })
