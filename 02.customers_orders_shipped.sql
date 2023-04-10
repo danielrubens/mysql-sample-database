9b3b39659561f5b7aa6ea5b72876e4df87cfad26
daniel <danielrubensdaniel@gmail.com>
2023-04-10 03:11:20 -0300

    change query02 column from orderDate to shippedDate

--- 02.customers_orders_shipped.sql
+++ 02.customers_orders_shipped.sql
@@ -1,7 +1,4 @@
--- SELECT customerNumber FROM classicmodels.orders WHERE YEAR(orderDate) = 2003 AND MONTH(orderDate) = 8;
-
 SELECT DISTINCT c.* 
 FROM classicmodels.customers c 
 INNER JOIN classicmodels.orders o ON c.customerNumber = o.customerNumber 
-WHERE YEAR(o.orderDate) = 2003 AND MONTH(o.orderDate) = 8;
-
+WHERE YEAR(o.shippedDate) = 2003 AND MONTH(o.shippedDate) = 8;
